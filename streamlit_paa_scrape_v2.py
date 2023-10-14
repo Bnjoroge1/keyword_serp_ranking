494ff1b69c7eeaa30183751570a866994fa91a28
cptntommy <141337672+cptntommy@users.noreply.github.com>
2023-10-14 21:05:24 +0100

    Create streamlit_paa_scrape_v2.py
    Modified the main value serp api keyword request to pull PAA questions

---
+++ streamlit_paa_scrape_v2.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import requests
+import os
+api_key = os.environ.get('API_KEY')
+
+# Title of the Streamlit app
+st.title("People Also Ask - Titles & URLs")
+
+# Input fields for user
+keywords_input = st.text_input("Please enter the keyword(s) (separated by commas if multiple):")
+google_domain = st.text_input("Please enter the Google domain (e.g. 'google.co.uk'):")
+gl = st.text_input("Please enter the Google country (e.g. 'uk'):")
+hl = st.text_input("Please enter the user UI language (e.g. 'en'):")
+
+# Button to trigger the API call
+if st.button('Get Results'):
+    # Splitting the keywords input by comma
+    keywords = [keyword.strip() for keyword in keywords_input.split(",")]
+
+    # Initialize the progress bar and status text
+    progress_bar = st.progress(0)
+    status_text = st.empty()
+
+    combined_csv_data = ""
+    for index, q in enumerate(keywords):
+        # Update the status text to inform the user which keyword is being processed
+        status_text.text(f"Fetching data for keyword: {q} ...")
+
+        # Set up the request parameters for each keyword
+        params = {
+            'api_key': api_key,
+            'q': q,
+            'google_domain': google_domain,
+            'gl': gl,
+            'hl': hl,
+            'max_page': '5',
+            'num': '100',
+            'output': 'csv',
+            'csv_fields': 'search.q,related_questions.question,related_questions.answer,related_questions.source.link'
+        }
+
+# Make the HTTP GET request to VALUE SERP
+        api_result = requests.get('https://api.valueserp.com/search', params)
+        csv_data = api_result.content.decode("utf-8")
+        combined_csv_data += csv_data + "\n"  # Add a newline between results of different keywords
+
+        # Update the progress bar
+        progress_bar.progress((index + 1) / len(keywords))
+
+    # Inform the user that the process is complete
+    status_text.text("Data fetching complete! You can now download the CSV.")
+
+    # Providing the combined CSV content as a downloadable file
+    st.download_button(
+        label="Download CSV",
+        data=combined_csv_data,
+        file_name="paa_scraped_data",
+        mime="text/csv"
+    )
