470ea8ef1a5f90f322ea629ba00f26b063657229
cptntommy <141337672+cptntommy@users.noreply.github.com>
2023-10-14 18:27:07 +0100

    Create streamlit_keyword_serp_ranking.py
    Added main code for streamlit app. This will use value serp to query for a specific keyword, and then get results for it in a csv format.

---
+++ streamlit_keyword_serp_ranking.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+import os
+api_key = os.environ.get('API_KEY')
+
+# Title of the Streamlit app
+st.title("Keyword SERP Ranking - Titles & URLs")
+
+# Input fields for user
+q = st.text_input("Please enter the keyword:")
+google_domain = st.text_input("Please enter the Google domain (e.g. 'google.co.uk'):")
+gl = st.text_input("Please enter the Google country (e.g. uk):")
+hl = st.text_input("Please enter the user UI language (e.g. en):")
+
+# Button to trigger the API call
+if st.button('Get Results'):
+    # Set up the request parameters
+    params = {
+        'api_key': api_key,
+        'q': q,
+        'google_domain': google_domain,
+        'gl': gl,
+        'hl': hl,
+        'max_page': '5',
+        'num': '100',
+        'output': 'csv',
+        'csv_fields': 'search.q,organic_results.position,organic_results.title,organic_results.link'
+    }
+
+    # Make the HTTP GET request to VALUE SERP
+    api_result = requests.get('https://api.valueserp.com/search', params)
+
+    # Display the result in Streamlit
+    st.write(api_result.content.decode("utf-8"))
