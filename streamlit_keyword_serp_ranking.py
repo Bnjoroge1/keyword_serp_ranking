bc1dc23465be9dd758a07d34528335c42c7c3858
cptntommy <141337672+cptntommy@users.noreply.github.com>
2023-12-14 11:14:51 +0000

    Toggle Between Manual & CSV
    Added a toggle to change between the manual input or a csv input, rather than having two seperate visible fields

--- streamlit_keyword_serp_ranking.py
+++ streamlit_keyword_serp_ranking.py
@@ -6,29 +6,30 @@ import pandas as pd
 # Function to read keywords from a CSV file
 def read_keywords_from_csv(file):
     df = pd.read_csv(file)
-    # Assuming the keywords are in the first column
     return df.iloc[:, 0].tolist()
 
 # Streamlit UI components
 api_key = os.environ.get('API_KEY')
-
-# Title of the Streamlit app
 st.title("Keyword SERP Ranking - Titles & URLs")
 
-# Input fields for user
-keywords_input = st.text_input("Please enter the keyword(s) (separated by commas if multiple):")
+# Radio button for input method
+input_method = st.radio("Choose your input method:", ('Manual Input', 'CSV Upload'))
+
+# Conditional input fields based on chosen input method
+if input_method == 'Manual Input':
+    keywords_input = st.text_input("Please enter the keyword(s) (separated by commas if multiple):")
+elif input_method == 'CSV Upload':
+    uploaded_file = st.file_uploader("Upload a CSV file with keywords (one per line):", type=["csv"])
+
 google_domain = st.text_input("Please enter the Google domain (e.g. 'google.co.uk'):")
 gl = st.text_input("Please enter the Google country (e.g. 'uk'):")
 hl = st.text_input("Please enter the user UI language (e.g. 'en'):")
 
-# File uploader for CSV
-uploaded_file = st.file_uploader("Upload a CSV file with keywords (one per line):", type=["csv"])
-
-# Determine the source of keywords: CSV or manual input
-if uploaded_file is not None:
-    keywords = read_keywords_from_csv(uploaded_file)
-elif keywords_input:
+# Determine the source of keywords
+if input_method == 'Manual Input' and keywords_input:
     keywords = [keyword.strip() for keyword in keywords_input.split(",")]
+elif input_method == 'CSV Upload' and uploaded_file is not None:
+    keywords = read_keywords_from_csv(uploaded_file)
 else:
     keywords = []
 
@@ -40,10 +41,7 @@ if st.button('Get Results') and keywords:
 
     combined_csv_data = ""
     for index, q in enumerate(keywords):
-        # Update the status text
         status_text.text(f"Fetching data for keyword: {q} ...")
-
-        # Set up the request parameters
         params = {
             'api_key': api_key,
             'q': q,
@@ -56,18 +54,12 @@ if st.button('Get Results') and keywords:
             'csv_fields': 'search.q,organic_results.position,organic_results.title,organic_results.link'
         }
 
-        # Make the HTTP GET request
         api_result = requests.get('https://api.valueserp.com/search', params)
         csv_data = api_result.content.decode("utf-8")
         combined_csv_data += csv_data + "\n"
-
-        # Update the progress bar
         progress_bar.progress((index + 1) / len(keywords))
 
-    # Completion message
     status_text.text("Data fetching complete! You can now download the CSV.")
-
-    # Download button
     st.download_button(
         label="Download CSV",
         data=combined_csv_data,
